85bcbe975f755e2b14a22ef60da03007bda078c8
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-01-15 00:33:07 +0530

    async code end of section 2

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -17,18 +19,18 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-             return  _context.Users.ToList();
+             return  await _context.Users.ToListAsync();
 
         }
 
 
         //  api/users/3
          [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id)
+        public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-             return  _context.Users.Find(id);
+             return  await _context.Users.FindAsync(id);
 
         }
     }
