8dc177fad08b7c91322523cd7483581d1cf15852
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-01-15 20:22:22 +0530

    End of section 3  HTTPS add SSL

--- API/Startup.cs
+++ API/Startup.cs
@@ -57,7 +57,7 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
