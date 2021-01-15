67d0e0eb941707f11903a04b2ef59064ae3a90d9
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-01-15 16:05:35 +0530

    making Http requests in Angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'The Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe(response =>{
+      this.users = response;
+    },error => {
+      console.log(error);
+      
+    })
+  }
 }
