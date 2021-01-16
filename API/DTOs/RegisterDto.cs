2822d13e5af8d8fc1eb157e46b1f9e6b74c72a70
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-01-16 23:21:32 +0530

    038 Adding validation

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDto
     {
+
+        [Required]
         public string UserName { get; set; }
 
+        [Required]
         public string Password { get; set; }
     }
 }
