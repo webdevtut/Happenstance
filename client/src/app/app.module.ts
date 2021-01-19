212fe57058f81db2fdff3762809f85ac0e3a664b
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-01-19 15:38:15 +0530

    049 Introduction to Angular template forms

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { NavComponent } from './nav/nav.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  model: any = {}
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    console.log(this.model);
+    
   }
 
 }
