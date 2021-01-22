6c167ce4b153ea11dd1b47709c342ce342037ef0
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-01-22 14:36:06 +0530

    074 Creating error controller for testing errors

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -9,6 +9,7 @@ namespace API.DTOs
         public string UserName { get; set; }
 
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
