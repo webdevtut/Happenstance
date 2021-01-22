16852491c441631ca9afb4391eb1e1182459772a
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-01-22 15:45:42 +0530

    075 Handling server errors

--- API/Startup.cs
+++ API/Startup.cs
@@ -50,12 +50,12 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            //     app.UseSwagger();
+            //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+            // }
 
             app.UseHttpsRedirection();
 
