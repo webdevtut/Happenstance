9f421d42572e77a3d4b9f1ba074f985e15ed00b2
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-01-27 23:52:50 +0530

    093 Adding a DTO for Members

---
+++ API/DTOs/PhotoDto.cs
@@ -0,0 +1,11 @@
+namespace API.DTOs
+{
+    public class PhotoDto
+    {
+        public int Id { get; set; }
+
+        public string  Url { get; set; }
+
+        public bool IsMain { get; set; }
+    }
+}
