c48b1bfc82d3159418c3c72812120bdca831908b
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-01-29 14:06:03 +0530

    103 Retrieving the list of members

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -6,7 +6,7 @@ import { Member } from '../_models/member';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
   })
 }
 
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-list',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberListComponent implements OnInit {
 
-  constructor() { }
+  members: Member[]
+
+  constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers(){
+    this,this.memberService.getMembers().subscribe(members => {
+      this.members = members;
+    })
   }
 
 }
