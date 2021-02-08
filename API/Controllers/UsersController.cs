25e2f353de7dcc3a441607bc34edba8a00478fe3
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-02-08 15:49:50 +0530

    136 Deleting photos - API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -112,5 +112,28 @@ namespace API.Controllers
             return BadRequest("Failed to set main photo");
         }
 
+        [HttpDelete("delete-photo/{photoId}")]
+        public async Task<ActionResult> DeletePhoto(int photoId)
+        {
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+            var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+
+            if (photo == null) return NotFound();
+
+            if(photo.IsMain) return BadRequest("You can not delete your main photo");
+
+            if (photo.PublicId != null)
+            {
+                var result = await _photoService.DeletePhotoAsync(photo.PublicId);
+                if (result.Error != null) return BadRequest(result.Error.Message);
+            }
+
+            user.Photos.Remove(photo);
+            if (await _userRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("Failed to delete the photo");
+        }
+
     }
 }
