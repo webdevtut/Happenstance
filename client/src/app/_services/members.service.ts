2f84d2a6ef70a361b129ade43016a7cf5fc4d6a0
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-02-08 15:35:02 +0530

    135 Setting the main photo in the client

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -10,14 +10,14 @@ import { Member } from '../_models/member';
   providedIn: 'root'
 })
 export class MembersService {
-  basrUrl = environment.apiUrl;
+  baseUrl = environment.apiUrl;
   members: Member[] = [];
 
   constructor(private http: HttpClient) { }
 
   getMembers() {
     if (this.members.length > 0) return of(this.members); 
-    return this.http.get<Member[]>(this.basrUrl + 'users').pipe(
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
       map(members => {
         this.members = members;
         return members;
@@ -27,14 +27,18 @@ export class MembersService {
   getMember(username: string ) {
     const member = this.members.find(x => x.username === username);
     if (member !== undefined) return of(member);
-    return this.http.get<Member[]>(this.basrUrl + 'users/' + username);
+    return this.http.get<Member[]>(this.baseUrl + 'users/' + username);
   }
   updateMember(member: Member){
-    return this.http.put(this.basrUrl + 'users', member).pipe(
+    return this.http.put(this.baseUrl + 'users', member).pipe(
       map(() => {
         const index = this.members.indexOf(member);
         this.members[index] = member;
       })
     )
+  }
+  setMainPhoto(photoId: number){
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
+
   }
 }
--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -2,8 +2,10 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
 import { take } from 'rxjs/operators';
 import { Member } from 'src/app/_models/member';
+import { Photo } from 'src/app/_models/photo';
 import { User } from 'src/app/_models/user';
 import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -18,7 +20,7 @@ export class PhotoEditorComponent implements OnInit {
   baseUrl = environment.apiUrl;
   user : User;
 
-  constructor(private accountService : AccountService) {
+  constructor(private accountService : AccountService, private memberService : MembersService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
    }
 
@@ -28,6 +30,18 @@ export class PhotoEditorComponent implements OnInit {
 
   fileOverBase(e: any){
     this.hasBaseDropeZone = e;
+  }
+
+  setMainPhoto(photo: Photo){
+    this,this.memberService.setMainPhoto(photo.id).subscribe(() =>{
+      this.user.photoUrl = photo.url;
+      this.accountService.setCurrentUser(this.user);
+      this.member.photoUrl = photo.url;
+      this.member.photos.forEach(p => {
+        if (p.isMain) p.isMain = false;
+        if (p.id === photo.id) p.isMain = true;
+      })
+    })
   }
 
   initializeUploader() {
