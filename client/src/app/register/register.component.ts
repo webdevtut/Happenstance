60bf07b7917c452389d5bc940a4d96742ea5f376
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-02-09 20:51:37 +0530

    146 Expanding the register form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -23,7 +23,12 @@ export class RegisterComponent implements OnInit {
 
   initializeForm(){
     this.registerForm = this.fb.group({
+      gender: ['male'],
       username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
       password: ['', [Validators.required, 
         Validators.minLength(4), Validators.maxLength(8)]],
       confirmPassword: ['', [Validators.required, this.matchValues('password')]]
