529f3b9cd0407dec42b26f19069bb015dca63437
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-02-10 12:05:02 +0530

    148 Updating the API register method

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs
@@ -7,6 +8,11 @@ namespace API.DTOs
 
         [Required]
         public string UserName { get; set; }
+
+        [Required]  public string KnownAs { get; set; }
+        [Required]  public string Gender { get; set; }
+        [Required]  public DateTime DateOfBirth { get; set; }
+        [Required]  public string Country { get; set; }
 
         [Required]
         [StringLength(8, MinimumLength = 4)]
