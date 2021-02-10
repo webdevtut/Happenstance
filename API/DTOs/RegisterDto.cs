3fd4edec560c2958dccceabbd1c3a74bd1011e31
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-02-10 13:25:09 +0530

    149 Client side registration ::End Of section 12::

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -12,6 +12,7 @@ namespace API.DTOs
         [Required]  public string KnownAs { get; set; }
         [Required]  public string Gender { get; set; }
         [Required]  public DateTime DateOfBirth { get; set; }
+        [Required]  public string City { get; set; }
         [Required]  public string Country { get; set; }
 
         [Required]
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -13,9 +14,10 @@ export class RegisterComponent implements OnInit {
   model : any = {};  
   registerForm: FormGroup;
   maxDate: Date;
+  validationErrors: string[] = [];
 
   constructor(private accountService : AccountService,
-    private toastr: ToastrService, private fb: FormBuilder) { }
+    private toastr: ToastrService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
     this.initializeForm();
@@ -45,15 +47,13 @@ export class RegisterComponent implements OnInit {
   }
 
   register (){
-    console.log(this.registerForm.value);
+    // console.log(this.registerForm.value);
     
-    // this.accountService.register(this.model).subscribe(response => {
-    //   console.log(response);
-    //   this.cancel();
-    // }, error => {
-    //   console.log(error);
-    //   this.toastr.error(error.error);
-    // })
+    this.accountService.register(this.registerForm.value).subscribe(response => {
+      this.router.navigateByUrl('/members')
+    }, error => {
+      this.validationErrors = error;
+    })
     
   }
   cancel (){
