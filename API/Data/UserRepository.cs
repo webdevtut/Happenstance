5dd8f91e33ac926367939ce166edc63c76a09446
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-02-11 18:31:55 +0530

    158 Adding additional filters

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -57,6 +58,11 @@ namespace API.Data
 
                 query = query.Where(u => u.UserName != userParams.CurrentUsername);
                 query = query.Where(u => u.Gender == userParams.Gender);
+                
+                var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+                var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+
+                query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
 
                 return await PagedList<MemberDto>.CreateAsync(query.ProjectTo<MemberDto>(_mapper
                     .ConfigurationProvider).AsNoTracking(), 
