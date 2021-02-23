60c34e88fadb6f156e72fd892ca25815b40f2aef
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-02-23 14:58:36 +0530

    195 Fixing the photo weirdness

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -11,9 +11,10 @@ import { MessageService } from '../_services/message.service';
 export class MessagesComponent implements OnInit {
   messages : Message[] = [];
   pagination!: Pagination;
-  container = 'Inbox';
+  container = 'Unread';
   pageNumber = 1;
   pageSize = 5;
+  loading = false;
 
   constructor(private messageService : MessageService) { }
 
@@ -22,10 +23,11 @@ export class MessagesComponent implements OnInit {
   }
   
   loadMessages(){
+    this.loading = true;
     this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe(response => {
       this.messages = response.result;
       this.pagination = response.pagination;
-
+      this.loading = false;
     })
   }
 
