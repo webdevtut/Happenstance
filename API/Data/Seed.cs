e7e8009a7510b8fc2a5248eafb9643773c2b8663
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-02-24 15:19:57 +0530

    206 Adding roles to the app

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -9,13 +9,27 @@ namespace API.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(UserManager<AppUser> userManager)
+        public static async Task SeedUsers(UserManager<AppUser> userManager, 
+            RoleManager<AppRole> roleManager)
         {
             if (await userManager.Users.AnyAsync()) return;
 
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
-            
+            if(users == null) return;
+
+            var roles = new List<AppRole>
+            {
+                new AppRole{Name = "Member"},
+                new AppRole{Name = "Admin"},
+                new AppRole{Name = "Moderator"},
+            };
+
+            foreach (var role in roles)
+            {
+                await roleManager.CreateAsync(role);
+            }
+
             foreach (var user in users)
             {
 
@@ -23,7 +37,17 @@ namespace API.Data
 
 
                 await userManager.CreateAsync(user, "Password@123");
+                await userManager.AddToRoleAsync(user, "Member");
             }
+
+            var admin = new AppUser
+            {
+                UserName = "admin"
+            };
+
+            await userManager.CreateAsync(admin, "Password@123");
+            await userManager.AddToRolesAsync(admin, new[] {"Admin", "Moderator"});
+
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -25,8 +25,9 @@ namespace API
            {
                var context = services.GetRequiredService<DataContext>();
                var userManager = services.GetRequiredService<UserManager<AppUser>>();
+               var roleManager =  services.GetRequiredService<RoleManager<AppRole>>();
                await context.Database.MigrateAsync();
-               await Seed.SeedUsers(userManager);
+               await Seed.SeedUsers(userManager, roleManager);
            }
            catch (Exception ex)
            {
