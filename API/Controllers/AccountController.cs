590eadbcd175cb0814ebf670925809b61a09fb1a
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-02-24 13:52:22 +0530

    205 Updating the account controller

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,14 +15,16 @@ namespace API.Controllers
 {
     public class AccountController : BaseApiController
     {
-        private readonly DataContext _context;
         private readonly ITokenService _tokenService;
         private readonly IMapper _mapper;
-        public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenService tokenService, IMapper mapper)
         {
+            _signInManager = signInManager;
+            _userManager = userManager;
             _mapper = mapper;
             _tokenService = tokenService;
-            _context = context;
         }
 
         [HttpPost("register")]
@@ -33,14 +36,14 @@ namespace API.Controllers
             var user = _mapper.Map<AppUser>(registerDto);
 
 
-                user.UserName = registerDto.UserName.ToLower();
+            user.UserName = registerDto.UserName.ToLower();
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+            if(!result.Succeeded) return BadRequest(result.Errors);
             
 
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
-
             return new UserDto
-            {
+            { 
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
                 KnownAs = user.KnownAs
@@ -50,12 +53,16 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users
+            var user = await _userManager.Users
             .Include(p => p.Photos)
-            .SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
+            .SingleOrDefaultAsync(x => x.UserName == loginDto.UserName.ToLower());
 
             if (user == null) return Unauthorized("Invalid Username");
 
+            var result = await _signInManager
+                .CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+                if (!result.Succeeded) return Unauthorized();
 
             return new UserDto
             {
@@ -70,7 +77,7 @@ namespace API.Controllers
         public async Task<bool> UserExists(string UserName)
         {
 
-            return await _context.Users.AnyAsync(x => x.UserName == UserName.ToLower());
+            return await _userManager.Users.AnyAsync(x => x.UserName == UserName.ToLower());
         }
     }
 }
