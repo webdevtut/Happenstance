66acce35b6ea2c64cb126f21777b7b8cbefe23c1
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-03-15 15:06:50 +0530

    240 Optimizing queries part one

--- API/Data/IMessageRepository.cs
+++ API/Data/IMessageRepository.cs
@@ -69,20 +69,20 @@ namespace API.Data
         {
             var query = _context.Messages
                 .OrderByDescending(m => m.MessageSent)
+                .ProjectTo<MessageDto>(_mapper.ConfigurationProvider)
                 .AsQueryable();
 
             query = messageParams.Container switch
             {
-                "Inbox" => query.Where(u => u.Recipient.UserName == messageParams.Username 
+                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username 
                         && u.RecipientDeleted == false),
-                "Outbox" => query.Where(u => u.Sender.UserName == messageParams.Username 
+                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username 
                         && u.SenderDeleted == false),
-                _ => query.Where(u => u.Recipient.UserName ==
+                _ => query.Where(u => u.RecipientUsername ==
                 messageParams.Username && u.RecipientDeleted == false && u.DateRead == null)
             };
             
-            var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
-            return await PagedList<MessageDto>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
+            return await PagedList<MessageDto>.CreateAsync(query, messageParams.PageNumber, messageParams.PageSize);
 
         }
 
@@ -90,8 +90,6 @@ namespace API.Data
             string recipientUsername)
         {
             var messages = await _context.Messages
-            .Include(u => u.Sender).ThenInclude(p => p.Photos)
-            .Include(u => u.Recipient).ThenInclude(p => p.Photos)
                 .Where(m => m.Recipient.UserName == currentUsername && m.RecipientDeleted == false
                         && m.Sender.UserName == recipientUsername
                         || m.Recipient.UserName == recipientUsername
@@ -99,10 +97,11 @@ namespace API.Data
                 
                 )
                 .OrderBy(m => m.MessageSent)
+                .ProjectTo<MessageDto>(_mapper.ConfigurationProvider)
                 .ToListAsync();
 
             var unreadMessages = messages.Where(m => m.DateRead == null 
-                && m.Recipient.UserName == currentUsername).ToList();
+                && m.RecipientUsername == currentUsername).ToList();
 
                 if(unreadMessages.Any())
                 {
@@ -112,7 +111,7 @@ namespace API.Data
                     }
                 }
 
-                return _mapper.Map<IEnumerable<MessageDto>>(messages);
+                return messages;
         }
 
         public void RemoveConnection(Connection connection)
