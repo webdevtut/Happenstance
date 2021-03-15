5ce233d70d04b65c38697e5aebfd6a96ebf45ee4
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-03-15 15:24:21 +0530

    241 Fixing UTC dates again

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -2,6 +2,10 @@ using Microsoft.EntityFrameworkCore;
 using API.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using System;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace API.Data
 {
@@ -60,7 +64,50 @@ namespace API.Data
             .WithMany(M => M.MessagesSent)
             .OnDelete(DeleteBehavior.Restrict);
 
-
+            builder.ApplyUtcDateTimeConverter();
         }
     }
+
+    public static class UtcDateAnnotation
+    {
+    private const String IsUtcAnnotation = "IsUtc";
+    private static readonly ValueConverter<DateTime, DateTime> UtcConverter =
+        new ValueConverter<DateTime, DateTime>(v => v, v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+
+    private static readonly ValueConverter<DateTime?, DateTime?> UtcNullableConverter =
+        new ValueConverter<DateTime?, DateTime?>(v => v, v => v == null ? v : DateTime.SpecifyKind(v.Value, DateTimeKind.Utc));
+
+    public static PropertyBuilder<TProperty> IsUtc<TProperty>(this PropertyBuilder<TProperty> builder, Boolean isUtc = true) =>
+        builder.HasAnnotation(IsUtcAnnotation, isUtc);
+
+    public static Boolean IsUtc(this IMutableProperty property) =>
+        ((Boolean?)property.FindAnnotation(IsUtcAnnotation)?.Value) ?? true;
+
+        /// <summary>
+            /// Make sure this is called after configuring all your entities.
+         /// </summary>
+    public static void ApplyUtcDateTimeConverter(this ModelBuilder builder)
+        {
+            foreach (var entityType in builder.Model.GetEntityTypes())
+        {
+        foreach (var property in entityType.GetProperties())
+        {
+            if (!property.IsUtc())
+                {
+                    continue;
+                }
+
+            if (property.ClrType == typeof(DateTime))
+                {
+                    property.SetValueConverter(UtcConverter);
+                }
+
+            if (property.ClrType == typeof(DateTime?))
+                {
+                    property.SetValueConverter(UtcNullableConverter);
+                }
+        }
+    }
+  }
+}
 }
