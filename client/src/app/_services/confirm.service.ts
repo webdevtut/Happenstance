aa2cc681a6e5bc37afbf005f6a21e5ef816c9e7f
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-03-16 13:52:16 +0530

    244 Getting the data from the confirmation modal

--- client/src/app/_services/confirm.service.ts
+++ client/src/app/_services/confirm.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { title } from 'process';
+import { Observable } from 'rxjs';
+import { ConfirmDialogComponent } from '../modals/confirm-dialog/confirm-dialog.component';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +12,10 @@ export class ConfirmService {
 
   constructor(private modalService : BsModalService) { }
 
-  confirm(tutle = 'Confirmation',
+  confirm(title = 'Confirmation',
     message = 'Are you sure you want to do this?',
     btnOkText = 'Ok',
-    btnCancelText = 'Cancel') {
+    btnCancelText = 'Cancel'): Observable<boolean> {
       const config = {
       initialState: {
         title,
@@ -22,6 +24,23 @@ export class ConfirmService {
         btnCancelText
       }
     }
-    this.bsModelRef = this.modalService.show('confirm', config);
+    this.bsModelRef = this.modalService.show(ConfirmDialogComponent, config);
+
+    return new Observable<boolean>(this.getResult());
+  }
+
+  private getResult(){
+    return(observer) => {
+      const subscription = this.bsModelRef.onHidden.subscribe(() => {
+        observer.next(this.bsModelRef.content.result);
+        observer.complete();
+      });
+
+      return {
+        unsubscribe() {
+          subscription.unsubscribe();
+        }
+      }
+    }
   }
 }
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Message } from '../_models/message';
 import { Pagination } from '../_models/pagination';
+import { ConfirmService } from '../_services/confirm.service';
 import { MessageService } from '../_services/message.service';
 
 @Component({
@@ -16,7 +17,7 @@ export class MessagesComponent implements OnInit {
   pageSize = 5;
   loading = false;
 
-  constructor(private messageService : MessageService) { }
+  constructor(private messageService : MessageService, private confirmService: ConfirmService) { }
 
   ngOnInit(): void {
     this.loadMessages();
@@ -32,8 +33,12 @@ export class MessagesComponent implements OnInit {
   }
 
   deleteMessages(id: number){
-    this.messageService.deleteMessage(id).subscribe(() => {
-      this.messages.splice(this.messages.findIndex(m => m.id === id), 1);
+    this.confirmService.confirm('Confirm Delete Message', 'this Cannot be undone').subscribe(result => {
+      if (result) {
+        this.messageService.deleteMessage(id).subscribe(() => {
+          this.messages.splice(this.messages.findIndex(m => m.id === id), 1);
+        })
+      }
     })
   }
   
