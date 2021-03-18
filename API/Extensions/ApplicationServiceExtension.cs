b07aaa9446d6ea3d38186d5918195d84c759e967
Tushar Kadam <tusharlookingfojob@gmail.com>
2021-03-18 18:41:37 +0530

    ssl

--- API/Extensions/ApplicationServiceExtension.cs
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -53,7 +53,7 @@ namespace API.Extensions
                         var pgHost = pgHostPort.Split(":")[0];
                         var pgPort = pgHostPort.Split(":")[1];
 
-                        connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
+                        connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};Trust Server Certificate=true";
                     }
 
             // Whether the connection string came from the local development configuration file
